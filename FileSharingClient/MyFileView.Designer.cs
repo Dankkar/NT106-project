0c81842c6334887e70edeb5383d4fd2fbd1271f7
h4rrydabrwn <huanphan2497@gmail.com>
2025-04-30 17:49:33 +0700

    Xong phan upload nhung chua sua owner_id

--- FileSharingClient/MyFileView.Designer.cs
+++ FileSharingClient/MyFileView.Designer.cs
@@ -39,19 +39,22 @@
             // 
             this.MyFileLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.MyFileLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.MyFileLayoutPanel.Location = new System.Drawing.Point(0, 50);
-            this.MyFileLayoutPanel.MaximumSize = new System.Drawing.Size(601, 342);
+            this.MyFileLayoutPanel.Location = new System.Drawing.Point(0, 92);
+            this.MyFileLayoutPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MyFileLayoutPanel.MaximumSize = new System.Drawing.Size(1102, 631);
             this.MyFileLayoutPanel.Name = "MyFileLayoutPanel";
-            this.MyFileLayoutPanel.Size = new System.Drawing.Size(601, 342);
+            this.MyFileLayoutPanel.Size = new System.Drawing.Size(1102, 631);
             this.MyFileLayoutPanel.TabIndex = 1;
+            this.MyFileLayoutPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.MyFileLayoutPanel_Paint);
             // 
             // lblFileName
             // 
             this.lblFileName.AutoSize = true;
             this.lblFileName.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFileName.Location = new System.Drawing.Point(32, 12);
+            this.lblFileName.Location = new System.Drawing.Point(59, 22);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblFileName.Name = "lblFileName";
-            this.lblFileName.Size = new System.Drawing.Size(53, 17);
+            this.lblFileName.Size = new System.Drawing.Size(92, 31);
             this.lblFileName.TabIndex = 2;
             this.lblFileName.Text = "Tên File";
             // 
@@ -59,9 +62,10 @@
             // 
             this.lblOwner.AutoSize = true;
             this.lblOwner.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOwner.Location = new System.Drawing.Point(181, 12);
+            this.lblOwner.Location = new System.Drawing.Point(332, 22);
+            this.lblOwner.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblOwner.Name = "lblOwner";
-            this.lblOwner.Size = new System.Drawing.Size(91, 17);
+            this.lblOwner.Size = new System.Drawing.Size(156, 31);
             this.lblOwner.TabIndex = 3;
             this.lblOwner.Text = "Người sở hữu";
             // 
@@ -69,9 +73,10 @@
             // 
             this.lblCreateAt.AutoSize = true;
             this.lblCreateAt.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCreateAt.Location = new System.Drawing.Point(306, 12);
+            this.lblCreateAt.Location = new System.Drawing.Point(561, 22);
+            this.lblCreateAt.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblCreateAt.Name = "lblCreateAt";
-            this.lblCreateAt.Size = new System.Drawing.Size(51, 17);
+            this.lblCreateAt.Size = new System.Drawing.Size(89, 31);
             this.lblCreateAt.TabIndex = 4;
             this.lblCreateAt.Text = "Tạo lúc";
             // 
@@ -79,15 +84,16 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(447, 12);
+            this.label1.Location = new System.Drawing.Point(820, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.Size = new System.Drawing.Size(123, 31);
             this.label1.TabIndex = 5;
             this.label1.Text = "Kích thước";
             // 
             // MyFileView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.Controls.Add(this.label1);
@@ -95,9 +101,9 @@
             this.Controls.Add(this.lblOwner);
             this.Controls.Add(this.lblFileName);
             this.Controls.Add(this.MyFileLayoutPanel);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MyFileView";
-            this.Size = new System.Drawing.Size(601, 392);
+            this.Size = new System.Drawing.Size(1102, 724);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- FileSharingClient/UploadView.Designer.cs
+++ FileSharingClient/UploadView.Designer.cs
@@ -32,89 +32,112 @@
             this.label1 = new System.Windows.Forms.Label();
             this.UploadedPanel = new System.Windows.Forms.Panel();
             this.btnUpload = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.UploadFilePanel = new System.Windows.Forms.FlowLayoutPanel();
             this.ListFiles = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
             this.DragPanel.SuspendLayout();
             this.UploadedPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // DragPanel
             // 
+            this.DragPanel.AllowDrop = true;
+            this.DragPanel.AutoScroll = true;
             this.DragPanel.Controls.Add(this.label1);
             this.DragPanel.Location = new System.Drawing.Point(3, 3);
             this.DragPanel.Name = "DragPanel";
-            this.DragPanel.Size = new System.Drawing.Size(400, 748);
+            this.DragPanel.Size = new System.Drawing.Size(367, 517);
             this.DragPanel.TabIndex = 0;
+            this.DragPanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragPanel_DragDrop);
+            this.DragPanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragPanel_DragEnter);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(45, 343);
+            this.label1.Location = new System.Drawing.Point(30, 146);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(177, 21);
+            this.label1.Size = new System.Drawing.Size(308, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kéo và thả File tại đây";
             // 
             // UploadedPanel
             // 
             this.UploadedPanel.Controls.Add(this.btnUpload);
-            this.UploadedPanel.Controls.Add(this.progressBar1);
-            this.UploadedPanel.Controls.Add(this.flowLayoutPanel1);
+            this.UploadedPanel.Controls.Add(this.UploadFilePanel);
             this.UploadedPanel.Controls.Add(this.ListFiles);
-            this.UploadedPanel.Location = new System.Drawing.Point(409, 3);
+            this.UploadedPanel.Location = new System.Drawing.Point(375, 3);
             this.UploadedPanel.Name = "UploadedPanel";
-            this.UploadedPanel.Size = new System.Drawing.Size(790, 748);
+            this.UploadedPanel.Size = new System.Drawing.Size(724, 718);
             this.UploadedPanel.TabIndex = 1;
             // 
             // btnUpload
             // 
             this.btnUpload.Font = new System.Drawing.Font("Segoe UI", 16F);
-            this.btnUpload.Location = new System.Drawing.Point(281, 634);
+            this.btnUpload.Location = new System.Drawing.Point(258, 609);
             this.btnUpload.Name = "btnUpload";
-            this.btnUpload.Size = new System.Drawing.Size(221, 81);
+            this.btnUpload.Size = new System.Drawing.Size(203, 78);
             this.btnUpload.TabIndex = 0;
             this.btnUpload.Text = "Upload";
             this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
             // 
-            // progressBar1
+            // UploadFilePanel
             // 
-            this.progressBar1.Location = new System.Drawing.Point(281, 575);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(221, 34);
-            this.progressBar1.TabIndex = 2;
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(16, 103);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(760, 456);
-            this.flowLayoutPanel1.TabIndex = 1;
+            this.UploadFilePanel.Location = new System.Drawing.Point(15, 99);
+            this.UploadFilePanel.Name = "UploadFilePanel";
+            this.UploadFilePanel.Size = new System.Drawing.Size(697, 438);
+            this.UploadFilePanel.TabIndex = 1;
             // 
             // ListFiles
             // 
             this.ListFiles.AutoSize = true;
             this.ListFiles.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
-            this.ListFiles.Location = new System.Drawing.Point(276, 42);
+            this.ListFiles.Location = new System.Drawing.Point(253, 40);
             this.ListFiles.Name = "ListFiles";
-            this.ListFiles.Size = new System.Drawing.Size(226, 25);
+            this.ListFiles.Size = new System.Drawing.Size(405, 46);
             this.ListFiles.TabIndex = 0;
             this.ListFiles.Text = "Các File đã được Upload";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label2.Location = new System.Drawing.Point(40, 541);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(290, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hoặc chọn File tại đây";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnBrowse.Location = new System.Drawing.Point(40, 601);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(283, 55);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
             // UploadView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.UploadedPanel);
             this.Controls.Add(this.DragPanel);
             this.Name = "UploadView";
-            this.Size = new System.Drawing.Size(1202, 754);
+            this.Size = new System.Drawing.Size(1102, 724);
             this.DragPanel.ResumeLayout(false);
             this.DragPanel.PerformLayout();
             this.UploadedPanel.ResumeLayout(false);
             this.UploadedPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -125,7 +148,8 @@
         private System.Windows.Forms.Panel UploadedPanel;
         private System.Windows.Forms.Label ListFiles;
         private System.Windows.Forms.Button btnUpload;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel UploadFilePanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
     }
 }
