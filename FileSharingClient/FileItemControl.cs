bf0866f8389466d69fad11cb88068986cdf11516
h4rrydabrwn <huanphan2497@gmail.com>
2025-05-12 10:14:20 +0700

    Sua lai Login, Register

--- FileSharingClient/FileItemControl.cs
+++ FileSharingClient/FileItemControl.cs
@@ -13,25 +13,13 @@ namespace FileSharingClient
 {
     public partial class FileItemControl : UserControl
     {
-<<<<<<< HEAD
-        private List<string> pendingFiles;
-        private UploadView parentView;
-
-=======
         public string FilePath { get; set; }
         public event Action<String> FileDeleted;
->>>>>>> develop
         public string FileName { get; set; }
         public string CreateAt { get; set; }
         public string Owner { get; set; }
         public string FileSize { get; set; }
-<<<<<<< HEAD
-       
-        // My File View
-        public FileItemControl(string filename, string createAt, string owner, string filesize)
-=======
         public FileItemControl(string filename, string createAt, string owner, string filesize, string filepath)
->>>>>>> develop
         {
             InitializeComponent();
 
@@ -46,22 +34,7 @@ namespace FileSharingClient
             btnMore.Click += (s, e) => contextMenuStrip1.Show(btnMore, new Point(0, btnMore.Height));
         }
 
-        // Upload View
-        public FileItemControl(string filename, string createAt, string owner, string filesize, List<string> pendingFiles, UploadView parentView)
-        {
-            InitializeComponent();// Lưu danh sách pendingFiles để thao tác
-            this.pendingFiles = pendingFiles ?? new List<string>();
-            this.parentView = parentView ?? throw new ArgumentNullException(nameof(parentView), "UploadView không the null");
 
-            // Set thông tin file vào control
-            lblFileName.Text = filename;
-            lblFileSize.Text = filesize;
-            lblOwner.Text = owner;
-            lblCreateAt.Text = createAt;
-
-            // Xử lý sự kiện cho nút More (Menu Chuột phải)
-            btnMore.Click += (s, e) => contextMenuStrip1.Show(btnMore, new Point(0, btnMore.Height));
-        }
 
         private void shareToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -73,26 +46,8 @@ namespace FileSharingClient
             DialogResult result = MessageBox.Show($"Bạn có chắc muốn xóa file {FileName}?", "Xác nhận", MessageBoxButtons.YesNo);
             if (result == DialogResult.Yes)
             {
-<<<<<<< HEAD
-                if (pendingFiles != null)
-                {
-                    var toRemove = pendingFiles.FirstOrDefault(p => Path.GetFileName(p) == FileName);
-                    if (toRemove != null)
-                    {
-                        pendingFiles.Remove(toRemove);
-                        UpdateTotalSize();
-                    }
-                    this.Dispose();
-                    parentView.UpdateTotalSize();
-                }
-                else
-                {
-                    MessageBox.Show("Lỗi: Không thể xóa file, danh sách pendingFiles không hợp lệ.");
-                }
-=======
                 FileDeleted?.Invoke(FilePath);
                 this.Dispose(); // Xóa FileItemControl khỏi giao diện
->>>>>>> develop
             }
         }
        
