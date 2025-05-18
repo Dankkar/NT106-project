3eb1bcb16f892ebc9bc3cadf91b70ff85ef9213f
h4rrydabrwn <huanphan2497@gmail.com>
2025-05-19 05:08:53 +0700

    Thay doi giao dien doi mat khau

--- FileSharingClient/ChangePassword.Designer.cs
+++ FileSharingClient/ChangePassword.Designer.cs
@@ -28,104 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChangePassword));
             this.label2 = new System.Windows.Forms.Label();
-            this.old_passtxtBox = new System.Windows.Forms.TextBox();
-            this.new_passtxtBox = new System.Windows.Forms.TextBox();
-            this.conf_new_passtxtBox = new System.Windows.Forms.TextBox();
-            this.view_pass = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.conf_new_passtxtBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.new_passtxtBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2ImageCheckBox1 = new Guna.UI2.WinForms.Guna2ImageCheckBox();
+            this.old_passtxtBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2ImageCheckBox2 = new Guna.UI2.WinForms.Guna2ImageCheckBox();
+            this.btnChangePassword = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.view_pass);
-            this.panel1.Controls.Add(this.conf_new_passtxtBox);
-            this.panel1.Controls.Add(this.new_passtxtBox);
-            this.panel1.Controls.Add(this.old_passtxtBox);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(328, 363);
-            this.panel1.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(93, 18);
+            this.label2.Location = new System.Drawing.Point(139, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(142, 30);
             this.label2.TabIndex = 9;
             this.label2.Text = "Đổi mật khẩu";
             // 
-            // old_passtxtBox
+            // guna2Panel1
             // 
-            this.old_passtxtBox.Location = new System.Drawing.Point(48, 86);
-            this.old_passtxtBox.Name = "old_passtxtBox";
-            this.old_passtxtBox.Size = new System.Drawing.Size(237, 20);
-            this.old_passtxtBox.TabIndex = 10;
-            // 
-            // new_passtxtBox
-            // 
-            this.new_passtxtBox.Location = new System.Drawing.Point(48, 131);
-            this.new_passtxtBox.Name = "new_passtxtBox";
-            this.new_passtxtBox.Size = new System.Drawing.Size(237, 20);
-            this.new_passtxtBox.TabIndex = 11;
+            this.guna2Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.guna2Panel1.Controls.Add(this.btnChangePassword);
+            this.guna2Panel1.Controls.Add(this.guna2ImageCheckBox2);
+            this.guna2Panel1.Controls.Add(this.guna2ImageCheckBox1);
+            this.guna2Panel1.Controls.Add(this.conf_new_passtxtBox);
+            this.guna2Panel1.Controls.Add(this.new_passtxtBox);
+            this.guna2Panel1.Controls.Add(this.old_passtxtBox);
+            this.guna2Panel1.Controls.Add(this.label2);
+            this.guna2Panel1.Location = new System.Drawing.Point(266, 51);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(428, 436);
+            this.guna2Panel1.TabIndex = 1;
             // 
             // conf_new_passtxtBox
             // 
-            this.conf_new_passtxtBox.Location = new System.Drawing.Point(48, 172);
+            this.conf_new_passtxtBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.conf_new_passtxtBox.DefaultText = "";
+            this.conf_new_passtxtBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.conf_new_passtxtBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.conf_new_passtxtBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.conf_new_passtxtBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.conf_new_passtxtBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.conf_new_passtxtBox.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.conf_new_passtxtBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.conf_new_passtxtBox.Location = new System.Drawing.Point(42, 255);
+            this.conf_new_passtxtBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.conf_new_passtxtBox.Name = "conf_new_passtxtBox";
-            this.conf_new_passtxtBox.Size = new System.Drawing.Size(237, 20);
-            this.conf_new_passtxtBox.TabIndex = 12;
+            this.conf_new_passtxtBox.PlaceholderText = "";
+            this.conf_new_passtxtBox.SelectedText = "";
+            this.conf_new_passtxtBox.Size = new System.Drawing.Size(350, 24);
+            this.conf_new_passtxtBox.TabIndex = 28;
             // 
-            // view_pass
+            // new_passtxtBox
             // 
-            this.view_pass.AutoSize = true;
-            this.view_pass.Location = new System.Drawing.Point(48, 213);
-            this.view_pass.Name = "view_pass";
-            this.view_pass.Size = new System.Drawing.Size(94, 17);
-            this.view_pass.TabIndex = 13;
-            this.view_pass.Text = "Xem mật khẩu";
-            this.view_pass.UseVisualStyleBackColor = true;
+            this.new_passtxtBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.new_passtxtBox.DefaultText = "";
+            this.new_passtxtBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.new_passtxtBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.new_passtxtBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.new_passtxtBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.new_passtxtBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.new_passtxtBox.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.new_passtxtBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.new_passtxtBox.Location = new System.Drawing.Point(42, 184);
+            this.new_passtxtBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.new_passtxtBox.Name = "new_passtxtBox";
+            this.new_passtxtBox.PlaceholderText = "";
+            this.new_passtxtBox.SelectedText = "";
+            this.new_passtxtBox.Size = new System.Drawing.Size(350, 24);
+            this.new_passtxtBox.TabIndex = 27;
             // 
-            // button1
+            // guna2ImageCheckBox1
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(98, 257);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 49);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "Đổi mật khẩu";
-            this.button1.UseVisualStyleBackColor = true;
+            this.guna2ImageCheckBox1.BackgroundImage = global::FileSharingClient.Properties.Resources.rsz_download;
+            this.guna2ImageCheckBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.guna2ImageCheckBox1.CheckedState.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image1")));
+            this.guna2ImageCheckBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageCheckBox1.Image")));
+            this.guna2ImageCheckBox1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageCheckBox1.ImageRotate = 0F;
+            this.guna2ImageCheckBox1.Location = new System.Drawing.Point(358, 184);
+            this.guna2ImageCheckBox1.Name = "guna2ImageCheckBox1";
+            this.guna2ImageCheckBox1.Size = new System.Drawing.Size(24, 24);
+            this.guna2ImageCheckBox1.TabIndex = 24;
+            // 
+            // old_passtxtBox
+            // 
+            this.old_passtxtBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.old_passtxtBox.DefaultText = "";
+            this.old_passtxtBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.old_passtxtBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.old_passtxtBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.old_passtxtBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.old_passtxtBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.old_passtxtBox.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.old_passtxtBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.old_passtxtBox.Location = new System.Drawing.Point(42, 109);
+            this.old_passtxtBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.old_passtxtBox.Name = "old_passtxtBox";
+            this.old_passtxtBox.PlaceholderText = "";
+            this.old_passtxtBox.SelectedText = "";
+            this.old_passtxtBox.Size = new System.Drawing.Size(350, 24);
+            this.old_passtxtBox.TabIndex = 15;
+            // 
+            // guna2ImageCheckBox2
+            // 
+            this.guna2ImageCheckBox2.BackgroundImage = global::FileSharingClient.Properties.Resources.rsz_download;
+            this.guna2ImageCheckBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.guna2ImageCheckBox2.CheckedState.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
+            this.guna2ImageCheckBox2.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageCheckBox2.Image")));
+            this.guna2ImageCheckBox2.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageCheckBox2.ImageRotate = 0F;
+            this.guna2ImageCheckBox2.Location = new System.Drawing.Point(358, 255);
+            this.guna2ImageCheckBox2.Name = "guna2ImageCheckBox2";
+            this.guna2ImageCheckBox2.Size = new System.Drawing.Size(24, 24);
+            this.guna2ImageCheckBox2.TabIndex = 29;
+            // 
+            // btnChangePassword
+            // 
+            this.btnChangePassword.BorderRadius = 18;
+            this.btnChangePassword.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnChangePassword.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnChangePassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnChangePassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnChangePassword.FillColor = System.Drawing.SystemColors.Highlight;
+            this.btnChangePassword.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnChangePassword.ForeColor = System.Drawing.Color.White;
+            this.btnChangePassword.Location = new System.Drawing.Point(133, 327);
+            this.btnChangePassword.Name = "btnChangePassword";
+            this.btnChangePassword.Size = new System.Drawing.Size(180, 45);
+            this.btnChangePassword.TabIndex = 30;
+            this.btnChangePassword.Text = "Đổi mật khẩu";
             // 
             // ChangePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.ClientSize = new System.Drawing.Size(352, 387);
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::FileSharingClient.Properties.Resources.blue_background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(940, 537);
+            this.Controls.Add(this.guna2Panel1);
             this.Name = "ChangePassword";
             this.Text = "ChangePassword";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox conf_new_passtxtBox;
-        private System.Windows.Forms.TextBox new_passtxtBox;
-        private System.Windows.Forms.TextBox old_passtxtBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.CheckBox view_pass;
-        private System.Windows.Forms.Button button1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2TextBox old_passtxtBox;
+        private Guna.UI2.WinForms.Guna2ImageCheckBox guna2ImageCheckBox1;
+        private Guna.UI2.WinForms.Guna2TextBox conf_new_passtxtBox;
+        private Guna.UI2.WinForms.Guna2TextBox new_passtxtBox;
+        private Guna.UI2.WinForms.Guna2ImageCheckBox guna2ImageCheckBox2;
+        private Guna.UI2.WinForms.Guna2Button btnChangePassword;
     }
 }
--- FileSharingClient/ChangePassword.cs
+++ FileSharingClient/ChangePassword.cs
@@ -16,5 +16,7 @@ namespace FileSharingClient
         {
             InitializeComponent();
         }
+
+        
     }
 }
